dbf8e96958da58c11d4502b5cec0de5e0c681297
nikitasetkin <nikitasetkin@gmail.com>
2026-08-09 12:08:29 +0000

    File changes

--- src/components/landing/Header.jsx
+++ src/components/landing/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
           className="flex items-center transition hover:text-primary"
         >
           <img
-            src="https://media.base44.com/images/public/6a25b90cc69d8cc1446d8488/90d052365_favicon7.png"
+            src="https://media.base44.com/images/public/6a25b90cc69d8cc1446d8488/19ece5429_favicon7-2.png"
             alt="Жар-птица"
             className="h-9 w-9 sm:h-10 sm:w-10 rounded-full object-cover mr-2"
           />
